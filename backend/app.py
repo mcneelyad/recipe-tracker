9aa2e3519d7344a4d8a8fdded01d5beae48136ea
mcneelyad <36091040+mcneelyad@users.noreply.github.com>
2022-04-30 14:02:20 -0400

    Add route to get recipe by recipe id

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,47 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-import flask_cors as cors
 from flask_mysqldb import MySQL
-
-import database as db
+import flask_cors as cors
+import os
 
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = os.environ.get('DB_HOST')
+app.config['MYSQL_USER'] = os.environ.get('DB_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('DB_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('DB')
 
-database = db.get_db(app)
+database = MySQL(app)
 
 @app.route('/')
-def index():
-    # return f'DB_PASSWORD = { app.config.get("DB_PASSWORD") }'
+def index():    
+    return { 'message': 'Hello World!' }
 
-    return jsonify(db.getRecipes(database))
+@app.route('/recipes')
+def get_recipes():
+    cur = database.connection.cursor()
+    cur.execute("SELECT * FROM recipe")
+    row_headers=[ x[0] for x in cur.description ]
+    result_data = cur.fetchall()
+    recipes = []
+    for result in result_data:
+        recipes.append(dict(zip(row_headers,result)))
+    return jsonify(recipes)
+
+@app.route('/recipes/<int:id>')
+def get_recipe_by_id(id):
+    cur = database.connection.cursor()
+    cur.execute("SELECT * FROM recipe WHERE id = %s", (id,))
+    row_headers=[ x[0] for x in cur.description ]
+    result_data = cur.fetchall()
+    if len(result_data) == 0:
+        return { 'message': 'Recipe not found' }
+    recipes = []
+    for result in result_data:
+        recipes.append(dict(zip(row_headers,result)))
+    print(recipes)
+    return jsonify(recipes)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,44 +12,24 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Ingredients</th>
+              <th>Instructions</th>
+              <th>Prep Time</th>
+              <th>Cook Time</th>
+              <th>Servings</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
       </main>
 
       <footer className={styles.footer}>
@@ -66,4 +46,12 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+ export async function getStaticProps() {
+  const data = await fetch('http://localhost:5000/recipes')
+  return {
+    props: {
+      data
+    },
+  }
 }
